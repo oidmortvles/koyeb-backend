81aad24068d23e1c4ac418878a7109c0f9d7683b
Daniel Pereyra <danielper.dev@gmail.com>
2022-10-22 02:09:18 -0300

    actualizacion de las clases Controller y AuthController

--- src/main/java/com/backend/backendportfolio/controller/Controller.java
+++ src/main/java/com/backend/backendportfolio/controller/Controller.java
@@ -3,6 +3,7 @@ package com.backend.backendportfolio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import com.backend.backendportfolio.service.UsuarioService;
 
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("")
 public class Controller {
 
